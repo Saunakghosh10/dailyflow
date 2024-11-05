fbb727453b6a29eb40a61ccc0f0066c40246f9d7
Saunak Ghosh <saunakghosh710@gmail.com>
2024-11-06 02:50:04 +0530

    build

--- next.config.mjs
+++ next.config.mjs
@@ -4,38 +4,13 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  redirects: async () => {
-    return [
-      {
-        source: '/daily-flow',
-        destination: '/',
-        permanent: true,
-      },
-    ];
+  typescript: {
+    ignoreBuildErrors: false,
   },
-  headers: async () => {
-    return [
-      {
-        source: '/manifest.json',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/manifest+json',
-          },
-        ],
-      },
-      {
-        // This allows same origin requests
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      },
-    ];
+  eslint: {
+    ignoreDuringBuilds: false,
   },
-};
+  poweredByHeader: false,
+}
 
 export default nextConfig;
